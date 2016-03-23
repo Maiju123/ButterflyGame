95b1e86ea1b5f4ee0cff47710d172ba706667c43
Maiju Schreck <K2322@student.jamk.fi>
2016-03-23 11:10:18 +0200

    Butterflygame, includes audio, keyboard controlling, mouse controlling, timer, game loop building, adding list to game, adding item(user smthg..)

---
+++ ButterflyGame/Butterfly.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ButterflyGame
+{
+    public sealed partial class Butterfly : UserControl
+    {
+        //Animate Butterfly
+        private DispatcherTimer timer;
+
+        //Offset to show
+        private int currentFrame = 0;
+        private int direction = 1; // 1 is direction down, -1 is up
+        private int frameHeight = 132;
+
+        //Location
+        public double LocationX { get; set; }
+        public double LocationY { get; set; }
+
+        // Speed
+        private readonly double MaxSpeed = 10.0;
+        private readonly double Accelerate = 0.5;
+        private double speed;
+
+        //Angle
+        private double Angle = 0;
+        private double AngleStep = 5;
+
+        public Butterfly()
+        {
+            this.InitializeComponent();
+
+            Animate();
+        }
+
+        //Animate Buttefly
+        private void Animate()
+        {
+            //Timer_Tick will be called in every 125 ms
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0,0,0,0,125);
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            // Current frame 0->4, 4->0, 0->4
+            if (direction == 1) currentFrame++;
+            else currentFrame--;
+            if (currentFrame == 0 || currentFrame == 4) direction *= -1; // 1 or -1 
+
+            //Offset
+            SpriteSheetOffset.Y = currentFrame * -frameHeight;
+        }
+
+        // Show Butterfly in Canvas (right location)
+        public void SetLocation ()
+        {
+            SetValue(Canvas.LeftProperty, LocationX);
+            SetValue(Canvas.TopProperty, LocationY);
+        }
+
+        // Move!!
+        public void Move()
+        {
+            //More speed
+            speed += Accelerate; // 0 -> 0.5 -> 1 > 1.5....
+            if (speed > MaxSpeed) speed = MaxSpeed; // MaxSpeed 10!
+            // Update location (with angle and speed)
+            LocationX -= (Math.Cos(Math.PI / 180 * (Angle + 90))) * speed;
+            LocationY -= (Math.Sin(Math.PI / 180 * (Angle + 90))) * speed;
+        }
+
+        // Rotate!!
+        public void Rotate(int angleDirection) // 1 or -1
+        {
+            Angle += AngleStep * angleDirection; //5 or -5
+            ButterflyRotateAngle.Angle = Angle;
+        }
+    }
+}
---
+++ ButterflyGame/MainPage.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ButterflyGame
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //Butterfly
+        private Butterfly butterfly;
+
+        //Flowers
+        private List<Flower> flowers = new List<Flower>();
+
+        //Audio
+        private MediaElement mediaElement;
+
+        // Game loop timer
+        private DispatcherTimer timer;
+
+        // Which keys are pressed
+        private bool UpPressed; // true, false
+        private bool RightPressed;
+        private bool LeftPressed;  
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //try open 800x600 window
+            ApplicationView.PreferredLaunchWindowingMode
+                = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.PreferredLaunchViewSize = new Size(800,600);
+
+            //Key listeners
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
+            Window.Current.CoreWindow.PointerPressed += CoreWindow_PointerPressed;
+            
+            
+            //Create Objects..
+            CreateButterfly();
+            StartGame();
+            InitAudio();
+
+        }
+
+        //Load audio from assests
+        private async void InitAudio()
+        {
+            mediaElement = new MediaElement();
+            mediaElement.AutoPlay = false;
+            StorageFolder folder =
+                await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
+            StorageFile file = await folder.GetFileAsync("tada.wav");
+            var stream = await file.OpenAsync(FileAccessMode.Read);
+            mediaElement.SetSource(stream, file.ContentType);
+
+        }
+
+        //Flowers
+        private void CoreWindow_PointerPressed(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.PointerEventArgs args)
+        {
+            //Create new Flower
+            Flower flower = new Flower();
+            flower.LocationX = args.CurrentPoint.Position.X - flower.Width / 2;
+            flower.LocationY = args.CurrentPoint.Position.Y - flower.Height / 2;
+            //Add to Canvas
+            MyCanvas.Children.Add(flower);
+            flower.SetLocation();
+            //Add flower to List
+            flowers.Add(flower);
+        }
+
+        //KeyDOWN
+        private void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Up:
+                    UpPressed = true;
+                    break;
+                case VirtualKey.Left:
+                    LeftPressed = true;
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+        //KeyUP
+        private void CoreWindow_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Up:
+                    UpPressed = false;
+                    break;
+                case VirtualKey.Left:
+                    LeftPressed = false;
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Create Butterfly
+        private void CreateButterfly()
+        {
+            butterfly = new Butterfly
+            {
+                LocationX = MyCanvas.Width / 2 - 75,
+                LocationY = MyCanvas.Height / 2 - 66
+            };
+            // Add Butterfly to Canvas
+            MyCanvas.Children.Add(butterfly);
+            //Show in right location
+            butterfly.SetLocation();
+        }
+
+        //Start game loop
+        private void StartGame()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0,0,0,0,1000/60); //60fps
+            timer.Start(); // Stop method similar way timer.Stop();!!
+        }
+
+        // Game loop
+        private void Timer_Tick(object sender, object e)
+        {
+            // Move Butterfly
+            if (UpPressed) butterfly.Move();
+            // Rotate Butterfly
+            //Debug.WriteLine(LeftPressed); // DEBUGGAUS
+            if (LeftPressed) butterfly.Rotate(-1);
+            if (RightPressed) butterfly.Rotate(1);
+            // Update Butterfly location
+            butterfly.SetLocation();
+            // Collision Butterfly with flowers
+            CheckCollission();
+
+        }
+        // Check collision with flowers and butterfly
+        private void CheckCollission()
+        {
+            foreach(Flower flower in flowers)
+            {
+                //Get rects
+                Rect r1 = new Rect(butterfly.LocationX,butterfly.LocationY, butterfly.ActualWidth,butterfly.ActualHeight); //Butterfly
+                Rect r2 = new Rect(flower.LocationX, flower.LocationY, flower.ActualWidth, butterfly.ActualHeight); //Flower
+                // Does intersects happend..? 
+                r1.Intersect(r2);
+                //If not empty
+                if (!r1.IsEmpty)
+                {
+                    //Play audio
+                    mediaElement.Play();
+                    // Remove flower
+                    MyCanvas.Children.Remove(flower);
+                    flowers.Remove(flower);
+                    break; // end looping..
+                }
+            }
+        }
+    }
+}
